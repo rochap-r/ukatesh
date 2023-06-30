7502e6dfa1239163bfc1230603faf7db05e8d0df
Rodrigue <rodriguechot@gmail.com>
2023-06-30 13:34:38 +0200

    Fixation du Vpt

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,10 +12,10 @@
     </script>
     <!--End Google tag (gtag.js) -->
     <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=yes"/>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Université Technologique Kanyik Tesh') </title>
+    <title>@yield('title',siteInfos()->sitename) </title>
     <link rel="apple-touch-icon" sizes="180x180" href="{{ siteInfos()->getAppleIcon18() }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ siteInfos()->getIcon48() }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ siteInfos()->getIcon16() }}">
